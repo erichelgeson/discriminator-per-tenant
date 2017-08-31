694b3488d45f030d33a38b93ce3438e2d740392c
Eric Helgeson <erichelgeson@gmail.com>
2017-08-30 22:06:33 -0500

    When unit testing with MT & HibernateSpec - having the gorm.* properties in application.groovy causes tests to fail

---
+++ complete/grails-app/conf/application.groovy
@@ -0,0 +1,8 @@
+//grails {
+//    gorm {
+//        multiTenancy = "DISCRIMINATOR"
+//        tenantResolverClass = "org.grails.datastore.mapping.multitenancy.web.SessionTenantResolver"
+//    }
+//}
+grails.gorm.multiTenancy = "DISCRIMINATOR"
+grails.gorm.tenantResolverClass = "org.grails.datastore.mapping.multitenancy.web.SessionTenantResolver"
